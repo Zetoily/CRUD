d742d1e43610682c9017f03b73c58ce4e50e5cde
Miron <miron.yusupov04@gmail.com>
2023-05-14 11:05:36 +0300

    CRUD2 VALIDATOR2

--- src/main/java/org/miron/springcourse/config/controllers/PeopleController.java
+++ src/main/java/org/miron/springcourse/config/controllers/PeopleController.java
@@ -5,7 +5,10 @@ import org.miron.springcourse.config.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -35,7 +38,12 @@ public class PeopleController {
         return "people/new";
     }
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person ,
+                         BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
 
@@ -46,8 +54,12 @@ public class PeopleController {
         return "people/edit";
     }
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person,
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
                          @PathVariable("id") int id){
+        if (bindingResult.hasErrors()){
+            return "people/edit";
+        }
         personDAO.update(id,person);
         return "redirect:/people";
     }
--- src/main/java/org/miron/springcourse/config/model/Person.java
+++ src/main/java/org/miron/springcourse/config/model/Person.java
@@ -1,9 +1,22 @@
 package org.miron.springcourse.config.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2,max = 30,message = "Name should be 2-30 chars")
     private String name;
+
+    @Min(value = 0,message = "Age should be >0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Not valid email")
     private String email;
 
     public Person() {
